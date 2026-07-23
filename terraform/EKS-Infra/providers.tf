4675739c8e6de1c87f4d44c47dcc8dd2f48d825b
Yogesh Bharambe <yogesh01bharambe@gmail.com>
2026-07-23 17:50:30 +0530

    Commiting the providers for eks cluster

--- terraform/EKS-Infra/providers.tf
+++ terraform/EKS-Infra/providers.tf
@@ -9,6 +9,19 @@ terraform {
   }
 }
 
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    # Require AWS CLI to get token dynamically
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
+}
+
+
 provider "helm" {
   kubernetes = {
     host                   = module.eks.cluster_endpoint
